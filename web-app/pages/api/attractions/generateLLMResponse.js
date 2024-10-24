11dc7e3bc136b72f1907f34054ef956379c53bd7
Eddison Pham <eddison.pham@mail.utoronto.ca>
2024-10-24 16:48:22 -0400

    api key

--- web-app/pages/api/attractions/generateLLMResponse.js
+++ web-app/pages/api/attractions/generateLLMResponse.js
@@ -3,10 +3,10 @@ const {GoogleGenerativeAI} = require("@google/generative-ai");
 export default async function handler(req,res){
     if (req.method === 'POST'){
         const {attractions_list ,date_range, purpose_of_trip, interests, preferences,other_info} = req.body
-        const genAI = new GoogleGenerativeAI("AIzaSyAuKIjmmBOmCeRhKsw-JqAFmgjgv0ZNJpo")
+        const genAI = new GoogleGenerativeAI(process.env.LLM_KEY)
         const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-        const prompt = "Explain how AI works";
+        const prompt = "Trip plan for 5 days starting today, In Toronto, say nothing else";
 
         const result = await model.generateContent(prompt);
         console.log(result.response.text());
