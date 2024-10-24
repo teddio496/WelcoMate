0374fc3d6c0bd82bd49c998f2bbe391b7f87f31e
Eddison Pham <eddison.pham@mail.utoronto.ca>
2024-10-24 17:41:52 -0400

    .

--- web-app/pages/api/attractions/generateLLMResponse.js
+++ web-app/pages/api/attractions/generateLLMResponse.js
@@ -6,7 +6,7 @@ export default async function handler(req,res){
         const genAI = new GoogleGenerativeAI(process.env.LLM_KEY)
         const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-        const prompt = "Trip plan for 5 days starting today, In Toronto, say nothing else";
+        const prompt = "Trip plan for 5 days starting today, In Toronto, say nothing else, in json format, give detailed description";
 
         const result = await model.generateContent(prompt);
         console.log(result.response.text());
