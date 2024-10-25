125ce1501a6a458dd4c433b197e532d244dd6289
Eddison Pham <eddison.pham@mail.utoronto.ca>
2024-10-24 21:22:32 -0400

    Completed Gemini LLM JSON formatter in generateLLMResponse

--- web-app/pages/api/attractions/generateLLMResponse.js
+++ web-app/pages/api/attractions/generateLLMResponse.js
@@ -1,12 +1,18 @@
 const {GoogleGenerativeAI} = require("@google/generative-ai");
-
 export default async function handler(req,res){
     if (req.method === 'POST'){
-        const {attractions_list ,date_range, purpose_of_trip, interests, preferences,other_info} = req.body
+        const {attractions_data ,weather_data, purpose_of_trip, interests, preferences,other_info} = req.body
+        console.log(attractions_data)
         const genAI = new GoogleGenerativeAI(process.env.LLM_KEY)
         const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-        const prompt = "Give a JSON response of the following and say nothing else: Given the days in the array "+date_range+", choose 3 attractions objects from "+attractions_list+" without replacement where the description is tailored based on: "+ purpose_of_trip+", "+interests+", "+preferences+", "+other_info+", and also satisfying the conditions: if the day's condition is rainy or thunderstorm, only pick attractions with the property that indoor_outdoor: Outdoor, otherwise pick any attractions. The JSON must follow the following format: {'day_1': {'morning': attraction_object1', 'afternoon': attraction_object2', 'night': attraction_object3}, ..., 'day_N': {...}}";
+        const prompt = "Give a JSON response, say nothing else. Use ONLY INFORMATION FROM the json "+JSON.stringify(attractions_data)+" dataset, which contains information on attractions. \
+        For each date in the JSON"+JSON.stringify(weather_data)+", select three attractions from attractions_data (without replacement) and assign them to the morning, afternoon, and night keys. \
+        If the weather condition for that date is not ideal, only select attractions that are indoor, otherwise, try to balance indoor and outdoor activities. For each attraction, \
+        include only the following keys: title, address, description, link_url, and indoor_outdoor. Tailor each description based on the context of the following prompts: \
+        "+purpose_of_trip+", "+interests+", "+preferences+", "+other_info+". The format of the JSON must follow this structure: { 'day_1': { 'morning': { 'title': '...', 'address': '...', \
+        'description': '...', 'link_url': '...'}, 'afternoon': { 'title': '...', 'address': '...', 'description': '...', 'link_url': '...'}, \
+        'night': { 'title': '...', 'address': '...', 'description': '...', 'link_url': '...'} }, 'day_N': { repeat the structure for other days } }";
 
         const result = await model.generateContent(prompt);
         console.log(result.response.text());
