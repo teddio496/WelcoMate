7f93fc767e740b080baf940e57e9d2fb156fb183
Oscar Pang <40321325+oskip0906@users.noreply.github.com>
2024-10-26 04:54:32 -0400

    last code fix

--- web-app/pages/api/generateTrip/index.js
+++ web-app/pages/api/generateTrip/index.js
@@ -56,6 +56,7 @@ export default async function handler(req, res) {
             }
             catch (error) {
                 console.log('Failed to parse response. Retrying...');
+                retries++;
             }   
         }
 
